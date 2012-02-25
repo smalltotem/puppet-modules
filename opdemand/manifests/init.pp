14f84d1e78f18bfe5889d0d726a38a27a8dc71b9
gabrtv <gabriel@opdemand.com>
2012-02-24 23:05:32 -0700

    initial pass at mysql module

--- opdemand/manifests/init.pp
+++ opdemand/manifests/init.pp
@@ -1,6 +1,6 @@
 # output to outputs.json 
 define opdemand::output($key, $value) {
-  exec {"/var/cache/opdemand/output $key $value":
+  exec {"/usr/local/bin/opdemand-output $key $value":
     path        => "/var/cache/opdemand",
     user        => "root",
     require =>  Class[Opdemand::Common],
