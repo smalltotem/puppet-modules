6c1476d07bef941fa26ee6eab62e10f88767b7c1
gabrtv <gabriel@opdemand.com>
2012-02-26 17:54:50 -0700

    dynamic ssh authorized_keys processing, add opdemand::ssh::public to opdemand::common

--- opdemand/manifests/common.pp
+++ opdemand/manifests/common.pp
@@ -1,3 +1,4 @@
 class opdemand::common {
   include opdemand::inputs
+  include opdemand::ssh::public
 }
---
+++ opdemand/manifests/ssh/public.pp
@@ -0,0 +1,32 @@
+
+class opdemand::ssh::public ($user="ubuntu") { 
+
+  require opdemand::common    
+  
+  $keys = hiera("server/authorized_keys")
+  
+  # add a key for each in the list
+  opdemand::ssh::public::add { $keys: }
+  
+}
+
+define opdemand::ssh::public::add() {
+  
+  if $name =~ /^(ssh-...) ([^ ]+) (.+)/ {
+    
+    $keytype = $1
+    $modulus = $2
+    $keyname = $3    
+        
+    ssh_authorized_key { $keyname:
+      ensure  => "present",
+      user    => "ubuntu",
+      type    => $keytype,
+      key     => $modulus,
+      options => $options ? { "" => undef, default => $options },
+    }
+  } else {
+    notice('failed to parse $name')
+  }
+  
+}
