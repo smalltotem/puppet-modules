a2c18403fdb40702c6c3f9b761479fb44e579270
gabrtv <gabriel@opdemand.com>
2012-03-20 14:31:08 -0600

    switch to include syntax

--- opdemand/manifests/framework/php.pp
+++ opdemand/manifests/framework/php.pp
@@ -6,8 +6,8 @@ class opdemand::framework::php {
   require opdemand::web::nginx
   
   # include nginx and php::nginx
-  nginx {"host":} ->
-  php::nginx {"host":}
+  include nginx
+  include php::nginx
 
   $packages = ["php5", "php5-cli", "php5-dev", "php5-curl", "php5-memcache", "php5-common", "php5-mysql", "php-pear", "php5-gd", "php5-idn", "php5-imagick", "php5-imap", "php5-mcrypt", "php5-mhash", "php5-ming", "php5-ps", "php5-pspell", "php5-recode", "php5-snmp", "php5-sqlite", "php5-tidy", "php5-xmlrpc", "php5-xsl", "php5-json", "php-apc"]
 
