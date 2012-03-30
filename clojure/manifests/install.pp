e1629594e78ffaee5dd4b71b55d1d7fce73bcbf1
dgriff1 <dan@opdemand.com>
2012-03-30 14:35:08 -0600

    add package

--- clojure/manifests/install.pp
+++ clojure/manifests/install.pp
@@ -1,6 +1,10 @@
 class clojure::install {
 
   require clojure::params
-  
+  $packages = ["leiningen"]
+  package { $packages:
+        ensure => latest,
+        require => $package_requires,
+  }
 
 }
