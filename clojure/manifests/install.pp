2b38487ebb91242413e850ffbd559fac1e6b03b3
dgriff1 <dan@opdemand.com>
2012-03-30 11:22:09 -0600

    update clojure puppet

--- clojure/manifests/install.pp
+++ clojure/manifests/install.pp
@@ -2,11 +2,5 @@ class clojure::install {
 
   require clojure::params
   
-  $package_requires = ["leiningen"]
-  
-  package { $packages:
-      ensure => latest,
-      require => $package_requires,
-  }
 
 }
