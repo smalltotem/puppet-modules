f5f0cd8fa72bf55faf11b7a82b09edbbcd541182
dgriff1 <dan@opdemand.com>
2012-04-02 16:36:16 -0600

    move stuff around

--- clojure/manifests/install.pp
+++ clojure/manifests/install.pp
@@ -2,9 +2,18 @@ class clojure::install {
 
   require clojure::params
   $packages = ["leiningen"]
-  package { $packages:
+  
+ package { $packages:
         ensure => latest,
   }
-
+  exec { "clojure::install::${name}":
+        command => "lein deps",
+        cwd => "/home/ubuntu/repo/",
+        path => ["/sbin", "/bin", "/usr/bin", "/usr/local/bin"],
+        user => "$clojure::params::username",
+        group => "$clojure::params::group",
+        subscribe => Vcsrepo["$clojure::params::repository_path"],
+  } 
+  
 
 }
